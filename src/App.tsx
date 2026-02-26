a01a689e33a13bb77238c7fb905377266cc252b9
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-02-26 17:27:15 +0200

    add tree render

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import TreeList from "./componets/TreeList";
+
+function App() {
+  return (
+    <div className="App">
+      <TreeList />
+    </div>
+  );
+}
+
+export default App;
