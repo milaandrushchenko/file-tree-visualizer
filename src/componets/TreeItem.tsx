a75e1aa755f3f96ad41991df08ba034629d23afe
Ludmila Andrushenko <ludmila.andrushenko@4k.com.ua>
2026-02-28 12:25:01 +0200

    feat: add expand/collapse functionality

--- src/componets/TreeItem.tsx
+++ src/componets/TreeItem.tsx
@@ -1,5 +1,6 @@
-import { FaFile, FaFolder } from "react-icons/fa";
+import { FaFile, FaFolder, FaFolderOpen } from "react-icons/fa";
 import type { FileSystemNode } from "../types";
+import { useState } from "react";
 
 export default function TreeItem({
   name,
@@ -8,25 +9,35 @@ export default function TreeItem({
   name: string;
   treeNode: FileSystemNode;
 }) {
+  const [expanded, setExpanded] = useState(false);
+
   if (treeNode.type === "file") {
     return (
-      <div style={{ paddingBottom: 8 }}>
-        <FaFile color="#4a90e2" /> {name}
+      <div className="file">
+        <FaFile color="#4a90e2" style={{ marginRight: 6 }} />
+        {name}
       </div>
     );
   }
 
   return (
     <div>
-      <div style={{ paddingBottom: 8 }}>
-        <FaFolder color="#f4b400" /> {name}
+      <div className="folder" onClick={() => setExpanded((v) => !v)}>
+        {expanded ? (
+          <FaFolderOpen color="#f4b400" />
+        ) : (
+          <FaFolder color="#f4b400" />
+        )}
+        <span>{name}</span>
       </div>
 
-      <div style={{ paddingLeft: 16 }}>
-        {Object.entries(treeNode.children).map(([key, value]) => (
-          <TreeItem key={key} name={key} treeNode={value} />
-        ))}
-      </div>
+      {expanded && (
+        <div className="item-children">
+          {Object.entries(treeNode.children).map(([key, value]) => (
+            <TreeItem key={key} name={key} treeNode={value} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
